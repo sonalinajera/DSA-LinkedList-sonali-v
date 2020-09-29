3ead7d21e95f211ab2fd06764584a49edf20b020
V <62076871+Nvlt@users.noreply.github.com>
2020-09-29 14:04:10 -0700

    meow

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const LinkedList  = require('./LinkedList.js');
+const BST = require('./BinarySearchTree');
 function displayLinkList(linkList)
 {
   for(let i = 0; i<=linkList.length(); i++)
@@ -10,25 +11,40 @@ function isEmpty(linkList)
 {
   return linkList.head == null;
 }
-function reverseLinkList(linkList)
-{
-  if(!linkList.head.next)
-  {
-    return linkList.head
-  }
-}
 
+
+function reverse(head) {
+  if (!head || !head.next) {
+    return head;
+  }                                  
+  let tmp = reverse(head.next);      
+  head.next.next = head;                            
+  head.next = undefined;
+  return tmp;
+}
+/* 
+  Head = 1 >> 2 >> 3 >> 4 | Head = 2 >> 3 >> 4 | Head = 3 >> 4 | Head = 4 Returned by if statement
+  Head = 3 >> 4 while tmp is a ref to 4 | Head.next.next(which is a ref to 4.next) = 3 >> 4 | Head.next(which is a ref to 3.next) = undefined.
+  tmp is now ref to 4, which now has a next that points to 3 | tmp gets returned,
+  Head = 2 >> 3 >> 4 while tmp is still a ref to 4 >> 3 since it was returned | head.next.next(which is a ref to 3.next) = 2 >> 3 >> 4 |
+  Head.next(which is a ref to 2.next) = undefined, tmp is now a ref to 4 >> 3 >> 2, and tmp is returned.
+  Head = 1 >> 2 >> 3 >> 4 while tmp is still a ref to 4 >> 3 >> 2 | head.next.next(which is a ref to 2.next) = 1 >> 2 >> 3 >> 4 |
+  Head.next(which is a ref to 1.next) = undefined, and tmp is now a ref to 4 >> 3 >> 2 >> 1 >> null
+  tmp is returned, the linked list is reversed. 
+*/
 
 function main () {
-  const list = new LinkedList()
-  list.insertLast("Meow1");
-  list.insertLast("Meow2");
-  list.insertLast("Meow3");
-  list.insertLast("Meow4");
-  //displayLinkList(list)
-  //console.log(list.pop())
-  reverseLinkList(list)
-  displayLinkList(list)
+  const binaryTree = new BST(8,"Primary meow");
+  binaryTree.insert(10, "meow 10");
+  binaryTree.insert(5, "meow 5");
+  binaryTree.insert(15, "meow 15");
+  binaryTree.insert(14, "meow 14");
+  binaryTree.insert(20, "meow 20");
+
+  binaryTree.remove(15);
+  console.log(binaryTree.findNode(20));
+
+  //while(1){}
   
 }
 
